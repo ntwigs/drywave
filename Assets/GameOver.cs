66997d321424aedef5c1e1ca705b0e2f573c13f2
ntwigs <oscar.cv.nordquist@gmail.com>
2018-11-11 15:26:49 +0100

    feat: Added script which kills the player and the rain if they collide

---
+++ Assets/GameOver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+	void OnCollisionEnter2D (Collision2D collider) {
+		if (collider.gameObject.name == "rain(Clone)") {
+			Destroy(collider.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
