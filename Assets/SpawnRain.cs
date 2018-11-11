b1802a981049763b48714cca686432731dae743c
ntwigs <oscar.cv.nordquist@gmail.com>
2018-11-11 14:38:33 +0100

    feat: Spawns rain at an interval

---
+++ Assets/SpawnRain.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRain : MonoBehaviour {
+
+	public GameObject rain;
+	public float spawnRate;
+	private float nextSpawn;
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawn) {
+			Instantiate(rain, new Vector3(0, 0), Quaternion.identity);
+			nextSpawn = Time.time + spawnRate;
+		}
+	}
+}
