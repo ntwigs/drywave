1fec32586ffb0fd3566bc1c6cc7a7e31a2d51d5a
ntwigs <oscar.cv.nordquist@gmail.com>
2018-11-11 14:58:37 +0100

    feat: Randomly positions the raindrops on spawn

--- Assets/SpawnRain.cs
+++ Assets/SpawnRain.cs
@@ -8,10 +8,25 @@ public class SpawnRain : MonoBehaviour {
 	public float spawnRate;
 	private float nextSpawn;
 
+	private float height;
+	private float width;
+
+	void Start () {
+		double verticalSize   = Camera.main.orthographicSize * 2.0;
+		double horizontalSize = verticalSize * Screen.width / Screen.height;	
+		height = (float)verticalSize;
+		width = (float)horizontalSize;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > nextSpawn) {
-			Instantiate(rain, new Vector3(0, 0), Quaternion.identity);
+
+			float spawnY = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y + 1;
+			float spawnX = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+			Vector2 spawnPosition = new Vector2(spawnX, spawnY);
+
+			Instantiate(rain, spawnPosition, Quaternion.identity);
 			nextSpawn = Time.time + spawnRate;
 		}
 	}
