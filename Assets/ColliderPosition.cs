5543a78b190fe1b94d7ba05168d2112e309d46e6
ntwigs <oscar.cv.nordquist@gmail.com>
2018-11-11 13:42:13 +0100

    feat: Places Colliders at each edge of the screen

---
+++ Assets/ColliderPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderPosition : MonoBehaviour {
+	public bool isRight = false;
+	public BoxCollider2D box;
+
+	void Start () {
+		double verticalSize   = Camera.main.orthographicSize * 2.0;
+		double horizontalSize = verticalSize * Screen.width / Screen.height;
+		float colliderSize = box.size.x;
+		if (isRight) {
+			float rightPosition = (float)horizontalSize / 2 + colliderSize / 2;
+			transform.position = new Vector2(rightPosition, transform.position.y);
+		} else {
+			float leftPosition = -(float)horizontalSize / 2 - colliderSize / 2;
+			transform.position = new Vector2(leftPosition, transform.position.y);
+		}
+	}
+}
