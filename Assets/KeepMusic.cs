183b9aec9ddc82a54fee93504b8166dbecd43cb8
ntwigs <oscar.cv.nordquist@gmail.com>
2018-11-28 19:13:24 +0100

    feat: Plays music across scene change

---
+++ Assets/KeepMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepMusic : MonoBehaviour {
+	private AudioSource audio;
+
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+		audio = GetComponent<AudioSource>();
+	}
+
+	public void PlayMusic()
+     {
+         if (audio.isPlaying) return;
+         audio.Play();
+     }
+ 
+     public void StopMusic()
+     {
+         audio.Stop();
+     }
+}
